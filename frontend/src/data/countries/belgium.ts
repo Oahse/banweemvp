da09ded82c83396a8b1d331fa819d5dd1e6a8f28
Oscar Oguledo <oscarchiagoziem@gmail.com>
2026-02-01 12:05:46 -0500

    fix: Remove duplicate Belgium country data file

--- frontend/src/data/countries/belgium.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Belgium country data with provinces, cities, and tax information
- */
-
-import { Country } from './index';
-
-export const belgium: Country = { code: 'BE', name: 'Belgium', taxInfo: { standardRate: 21, taxName: 'BTW', currency: 'EUR', region: 'EU' } };
-
-export default belgium;
