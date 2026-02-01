6b6c4e21dc2ee3e9297ac54f18afd86ddb6ba34d
Oscar Oguledo <oscarchiagoziem@gmail.com>
2026-02-01 12:55:51 -0500

    chore: Update countries index

--- frontend/src/data/countries/index.ts
+++ frontend/src/data/countries/index.ts
@@ -3,6 +3,285 @@
  * Comprehensive list of 500+ countries with tax information
  */
 
+// Import all country data
+import { afghanistan } from './afghanistan';
+import { albania } from './albania';
+import { algeria } from './algeria';
+import { americansamoa } from './american-samoa';
+import { andorra } from './andorra';
+import { angola } from './angola';
+import { anguilla } from './anguilla';
+import { antarctica } from './antarctica';
+import { antiguaandbarbuda } from './antigua-and-barbuda';
+import { argentina } from './argentina';
+import { armenia } from './armenia';
+import { aruba } from './aruba';
+import { australia } from './australia';
+import { austria } from './austria';
+import { azerbaijan } from './azerbaijan';
+import { bahamas } from './bahamas';
+import { bahrain } from './bahrain';
+import { bangladesh } from './bangladesh';
+import { barbados } from './barbados';
+import { belarus } from './belarus';
+import { belgium } from './belgium';
+import { belize } from './belize';
+import { benin } from './benin';
+import { bermuda } from './bermuda';
+import { bhutan } from './bhutan';
+import { bolivia } from './bolivia';
+import { bosniaandherzegovina } from './bosnia-and-herzegovina';
+import { botswana } from './botswana';
+import { brazil } from './brazil';
+import { britishindianoceanterritory } from './british-indian-ocean-territory';
+import { britishvirginislands } from './british-virgin-islands';
+import { brunei } from './brunei';
+import { bulgaria } from './bulgaria';
+import { burkinafaso } from './burkina-faso';
+import { burundi } from './burundi';
+import { caboverde } from './cabo-verde';
+import { cambodia } from './cambodia';
+import { cameroon } from './cameroon';
+import { canada } from './canada';
+import { caribbeannetherlands } from './caribbean-netherlands';
+import { caymanislands } from './cayman-islands';
+import { centralafricanrepublic } from './central-african-republic';
+import { chad } from './chad';
+import { chile } from './chile';
+import { china } from './china';
+import { colombia } from './colombia';
+import { comoros } from './comoros';
+import { congoBrazzaville } from './congo-brazzaville';
+import { congoKinshasa } from './congo-kinshasa';
+import { cookislands } from './cook-islands';
+import { costarica } from './costa-rica';
+import { coteDIvoire } from './cote-divoire';
+import { croatia } from './croatia';
+import { cuba } from './cuba';
+import { curaao } from './cura-ao';
+import { cyprus } from './cyprus';
+import { czechia } from './czechia';
+import { denmark } from './denmark';
+import { djibouti } from './djibouti';
+import { dominica } from './dominica';
+import { dominicanrepublic } from './dominican-republic';
+import { easttimor } from './east-timor';
+import { ecuador } from './ecuador';
+import { egypt } from './egypt';
+import { elsalvador } from './el-salvador';
+import { equatorialGuinea } from './equatorial-guinea';
+import { eritrea } from './eritrea';
+import { estonia } from './estonia';
+import { eswatini } from './eswatini';
+import { ethiopia } from './ethiopia';
+import { falklandislands } from './falkland-islands';
+import { faroeislands } from './faroe-islands';
+import { fiji } from './fiji';
+import { finland } from './finland';
+import { france } from './france';
+import { frenchguiana } from './french-guiana';
+import { frenchpolynesia } from './french-polynesia';
+import { gabon } from './gabon';
+import { gambia } from './gambia';
+import { georgia } from './georgia';
+import { germany } from './germany';
+import { ghana } from './ghana';
+import { greece } from './greece';
+import { greenland } from './greenland';
+import { grenada } from './grenada';
+import { guadeloupe } from './guadeloupe';
+import { guatemala } from './guatemala';
+import { guernsey } from './guernsey';
+import { guineabissau } from './guinea-bissau';
+import { guinea } from './guinea';
+import { guyana } from './guyana';
+import { haiti } from './haiti';
+import { honduras } from './honduras';
+import { hongkong } from './hong-kong';
+import { hungary } from './hungary';
+import { iceland } from './iceland';
+import { india } from './india';
+import { indonesia } from './indonesia';
+import { iran } from './iran';
+import { iraq } from './iraq';
+import { ireland } from './ireland';
+import { isleofman } from './isle-of-man';
+import { israel } from './israel';
+import { italy } from './italy';
+import { jamaica } from './jamaica';
+import { japan } from './japan';
+import { jersey } from './jersey';
+import { jordan } from './jordan';
+import { kazakhstan } from './kazakhstan';
+import { kenya } from './kenya';
+import { kiribati } from './kiribati';
+import { kuwait } from './kuwait';
+import { kyrgyzstan } from './kyrgyzstan';
+import { landislands } from './land-islands';
+import { laos } from './laos';
+import { latvia } from './latvia';
+import { lebanon } from './lebanon';
+import { lesotho } from './lesotho';
+import { liberia } from './liberia';
+import { libya } from './libya';
+import { liechtenstein } from './liechtenstein';
+import { lithuania } from './lithuania';
+import { luxembourg } from './luxembourg';
+import { macau } from './macau';
+import { madagascar } from './madagascar';
+import { malawi } from './malawi';
+import { malaysia } from './malaysia';
+import { maldives } from './maldives';
+import { mali } from './mali';
+import { malta } from './malta';
+import { marshallislands } from './marshall-islands';
+import { martinique } from './martinique';
+import { mauritania } from './mauritania';
+import { mauritius } from './mauritius';
+import { mayotte } from './mayotte';
+import { mexico } from './mexico';
+import { micronesia } from './micronesia';
+import { moldova } from './moldova';
+import { monaco } from './monaco';
+import { mongolia } from './mongolia';
+import { montenegro } from './montenegro';
+import { montserrat } from './montserrat';
+import { morocco } from './morocco';
+import { mozambique } from './mozambique';
+import { myanmar } from './myanmar';
+import { namibia } from './namibia';
+import { nauru } from './nauru';
+import { nepal } from './nepal';
+import { netherlands } from './netherlands';
+import { newcaledonia } from './new-caledonia';
+import { newzealand } from './new-zealand';
+import { nicaragua } from './nicaragua';
+import { niger } from './niger';
+import { nigeria } from './nigeria';
+import { niue } from './niue';
+import { northkorea } from './north-korea';
+import { northmacedonia } from './north-macedonia';
+import { northernmarianaislands } from './northern-mariana-islands';
+import { norway } from './norway';
+import { oman } from './oman';
+import { pakistan } from './pakistan';
+import { palau } from './palau';
+import { palestine } from './palestine';
+import { panama } from './panama';
+import { papuanewguinea } from './papua-new-guinea';
+import { paraguay } from './paraguay';
+import { peru } from './peru';
+import { philippines } from './philippines';
+import { pitcairnislands } from './pitcairn-islands';
+import { poland } from './poland';
+import { portugal } from './portugal';
+import { puertorico } from './puerto-rico';
+import { qatar } from './qatar';
+import { runion } from './r-union';
+import { romania } from './romania';
+import { russia } from './russia';
+import { rwanda } from './rwanda';
+import { saintbarthlemy } from './saint-barth-lemy';
+import { sainthelena } from './saint-helena';
+import { saintkittsandnevis } from './saint-kitts-and-nevis';
+import { saintlucia } from './saint-lucia';
+import { saintmartin } from './saint-martin';
+import { saintpierreandmiquelon } from './saint-pierre-and-miquelon';
+import { saintvincentandthegrenadines } from './saint-vincent-and-the-grenadines';
+import { samoa } from './samoa';
+import { sanmarino } from './san-marino';
+import { saoTomePrincipe } from './sao-tome-principe';
+import { saudiarabia } from './saudi-arabia';
+import { senegal } from './senegal';
+import { serbia } from './serbia';
+import { seychelles } from './seychelles';
+import { sierraleone } from './sierra-leone';
+import { singapore } from './singapore';
+import { sintmaarten } from './sint-maarten';
+import { slovakia } from './slovakia';
+import { slovenia } from './slovenia';
+import { solomonislands } from './solomon-islands';
+import { somalia } from './somalia';
+import { southafrica } from './south-africa';
+import { southkorea } from './south-korea';
+import { southsudan } from './south-sudan';
+import { spain } from './spain';
+import { srilanka } from './sri-lanka';
+import { sudan } from './sudan';
+import { suriname } from './suriname';
+import { svalbardandjanmayen } from './svalbard-and-jan-mayen';
+import { sweden } from './sweden';
+import { switzerland } from './switzerland';
+import { syria } from './syria';
+import { taiwan } from './taiwan';
+import { tajikistan } from './tajikistan';
+import { tanzania } from './tanzania';
+import { thailand } from './thailand';
+import { togo } from './togo';
+import { tokelau } from './tokelau';
+import { tonga } from './tonga';
+import { trinidadandtobago } from './trinidad-and-tobago';
+import { tunisia } from './tunisia';
+import { turkey } from './turkey';
+import { turkmenistan } from './turkmenistan';
+import { turksandcaicosislands } from './turks-and-caicos-islands';
+import { tuvalu } from './tuvalu';
+import { usvirginislands } from './u-s-virgin-islands';
+import { uganda } from './uganda';
+import { ukraine } from './ukraine';
+import { unitedarabemirates } from './united-arab-emirates';
+import { unitedStates } from './united-states';
+import { uruguay } from './uruguay';
+import { uzbekistan } from './uzbekistan';
+import { vanuatu } from './vanuatu';
+import { vaticancity } from './vatican-city';
+import { venezuela } from './venezuela';
+import { vietnam } from './vietnam';
+import { wallisandfutuna } from './wallis-and-futuna';
+import { westernsahara } from './western-sahara';
+import { yemen } from './yemen';
+import { zambia } from './zambia';
+import { zimbabwe } from './zimbabwe';
+
+// Main countries array
+export const countries: Country[] = [
+  afghanistan, albania, algeria, americansamoa, andorra, angola, anguilla, antarctica,
+  antiguaandbarbuda, argentina, armenia, aruba, australia, austria, azerbaijan,
+  bahamas, bahrain, bangladesh, barbados, belarus, belgium, belize, benin,
+  bermuda, bhutan, bolivia, bosniaandherzegovina, botswana, brazil,
+  britishindianoceanterritory, britishvirginislands, brunei, bulgaria,
+  burkinafaso, burundi, caboverde, cambodia, cameroon, canada, caribbeannetherlands,
+  caymanislands, centralafricanrepublic, chad, chile, china, colombia, comoros,
+  congoBrazzaville, congoKinshasa, cookislands, costarica, coteDIvoire, croatia, cuba,
+  curaao, cyprus, czechia, denmark, djibouti, dominica, dominicanrepublic,
+  easttimor, ecuador, egypt, elsalvador, equatorialGuinea, eritrea, estonia,
+  eswatini, ethiopia, falklandislands, faroeislands, fiji, finland, france,
+  frenchguiana, frenchpolynesia, gabon, gambia, georgia, germany, ghana, greece,
+  greenland, grenada, guadeloupe, guatemala, guernsey, guineabissau, guinea, guyana,
+  haiti, honduras, hongkong, hungary, iceland, india, indonesia, iran, iraq,
+  ireland, isleofman, israel, italy, jamaica, japan, jersey, jordan, kazakhstan,
+  kenya, kiribati, kuwait, kyrgyzstan, landislands, laos, latvia, lebanon,
+  lesotho, liberia, libya, liechtenstein, lithuania, luxembourg, macau,
+  madagascar, malawi, malaysia, maldives, mali, malta, marshallislands,
+  martinique, mauritania, mauritius, mayotte, mexico, micronesia, moldova,
+  monaco, mongolia, montenegro, montserrat, morocco, mozambique, myanmar,
+  namibia, nauru, nepal, netherlands, newcaledonia, newzealand, nicaragua,
+  niger, nigeria, niue, northkorea, northmacedonia, northernmarianaislands,
+  norway, oman, pakistan, palau, palestine, panama, papuanewguinea, paraguay,
+  peru, philippines, pitcairnislands, poland, portugal, puertorico, qatar, runion,
+  romania, russia, rwanda, saintbarthlemy, sainthelena, saintkittsandnevis,
+  saintlucia, saintmartin, saintpierreandmiquelon, saintvincentandthegrenadines,
+  samoa, sanmarino, saoTomePrincipe, saudiarabia, senegal, serbia, seychelles,
+  sierraleone, singapore, sintmaarten, slovakia, slovenia, solomonislands,
+  somalia, southafrica, southkorea, southsudan, spain, srilanka, sudan,
+  suriname, svalbardandjanmayen, sweden, switzerland, syria, taiwan, tajikistan,
+  tanzania, thailand, togo, tokelau, tonga, trinidadandtobago, tunisia, turkey,
+  turkmenistan, turksandcaicosislands, tuvalu, usvirginislands, uganda, ukraine,
+  unitedarabemirates, unitedStates, uruguay, usminoroutlyingislands, uzbekistan,
+  vanuatu, vaticancity, venezuela, vietnam, wallisandfutuna, westernsahara,
+  yemen, zambia, zimbabwe
+];
+
 /**
  * Country interface using the official division names for each country
  * Uses real administrative division types (e.g., "parish", "province", "state", "national district")
